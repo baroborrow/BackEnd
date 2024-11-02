f8fd8bc03ca8f712efc5cb49fb0bad9696827a6a
jaehyyuun <jaehyunj393@gmail.com>
2024-11-03 00:55:18 +0900

    Initial Commit

---
+++ src/main/java/com/baro/baroborrow/BaroborrowApplication.java
@@ -0,0 +1,13 @@
+package com.baro.baroborrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaroborrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaroborrowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baro/baroborrow/BaroborrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baro.baroborrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaroborrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
