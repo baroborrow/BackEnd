bb6a2ec8693e9689d14e207ef051b683b5f873bd
Bokjihee <bokjihee@naver.com>
2024-11-03 04:08:25 +0900

    fit: id없음 500 error 해결

--- src/main/java/com/baro/baroborrow/Service/PointService.java
+++ src/main/java/com/baro/baroborrow/Service/PointService.java
@@ -1,6 +1,7 @@
 package com.baro.baroborrow.Service;
 
 import com.baro.baroborrow.Domain.Point;
+import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
@@ -16,18 +17,18 @@ public class PointService {
 
         //User 테이블에서 현재 user 불러오기
         CollectionReference userCollectionRef = firestore.collection("User");
-        QuerySnapshot userQuerySnapshot = userCollectionRef.whereEqualTo("user_id", userId).get().get();
+        ApiFuture<QuerySnapshot> userQuerySnapshot = userCollectionRef.whereEqualTo("user_id", userId).get();
 
-        if (userQuerySnapshot.isEmpty()) {
+        if (userQuerySnapshot.get().isEmpty()) {
             throw new NoSuchElementException("User not found for userId: " + userId);
         }
 
         //Point 테이블에서 현재 user의 point 정보 모두 불러오기
         CollectionReference pointCollectionRef = firestore.collection("Point");
-        QuerySnapshot pointQuerySnapshot = pointCollectionRef.whereEqualTo("user_id", userId).get().get();
+        ApiFuture<QuerySnapshot> pointQuerySnapshot = pointCollectionRef.whereEqualTo("user_id", userId).get();
 
         List<Point> points = new ArrayList<>();
-        for (QueryDocumentSnapshot document : pointQuerySnapshot.getDocuments()) {
+        for (QueryDocumentSnapshot document : pointQuerySnapshot.get().getDocuments()) {
             points.add(document.toObject(Point.class));
         }
 
